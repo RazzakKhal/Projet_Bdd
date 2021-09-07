9333acf7200de47d85b31ac196a3b469ccc59b2e
KHALFALLAH Razzak <khalfallah.razzak@gmail.com>
2021-09-07 22:55:16 +0200

    Premier commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scalre=1.0">
+      <meta charset="utf-8">
+      <link href="index.css" rel="stylesheet">
+      <title>Projet 1 Back-end</title>
+      <meta name="description" content="Mon premier projet perso Back-end, le but est d'apprendre à créer une interface de connexion et d'inscriton reliée à une BDD">
+    </head>
+    <body>
+      <script src="index.js"></script>
+    </body>
+</html>
