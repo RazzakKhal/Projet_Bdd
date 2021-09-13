c5bda14c9734a5a02ffb5797eca8c7db5ba695c5
KHALFALLAH Razzak <khalfallah.razzak@gmail.com>
2021-09-14 01:11:13 +0200

    ajout de la regex pour le mot de passe sécurisé ainsi que de la condition avec preg match

--- verifinscription.php
+++ verifinscription.php
@@ -1,15 +1,31 @@
 <?php
 
-$pattern = '/@+/';
+$pattern = "/@+/";
 $verifmail = htmlspecialchars($_POST['mail']); //htmlspecialchars pour eviter failles XSS
 $verifpseudo = htmlspecialchars($_POST['pseudo']);
 $vfpass = htmlspecialchars($_POST['pass']);
 $vfpass2 = htmlspecialchars($_POST['pass2']);
 $verifpass=password_hash($vfpass,PASSWORD_BCRYPT);
 
-// On vérifie si les mots de passe entrées sont identiques et si le mail contient un @
+// Je veux que mes mot de passe contiennent au moins 8 caractères dont 1 caractère special, 1 majuscule, 1 chiffre
 
-if($vfpass === $vfpass2){
+$patt = "/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$/";
+
+// \S pour enlever les espaces en debut et fin de mdp, ?= correspond au lookahead (suivie de)
+//  ancré au début de la chaîne
+//  tout ensemble de caractères
+//  d'au moins 8
+//  contenant au moins une lettre minuscule
+//  et au moins une lettre majuscule
+//  et au moins un chiffre
+//  et au moins un caractère special
+//  $: ancré à la fin de la chaîne
+
+
+
+if(preg_match($patt, $_POST['pass'])){
+
+if($vfpass === $vfpass2){ // On vérifie si les mots de passe entrées sont identiques et si le mail contient un @
     if(preg_match($pattern, $verifmail)){
         //si c'est bon On vérifie que le login ou le mail n'existe pas deja dans la bdd
 
@@ -64,4 +80,7 @@ else{
     echo ' vos mot de passes de sembles pas identiques';
 }
 
-
+}
+else{
+    echo 'format du mot de passe incorrect, il doit contenir au moins 8 caractères dont 1 majuscule, 1 minuscule, un caractère special et 1 chiffre.';
+}
